c1c5038f4b328a6597da92c30bda4dc51cc650e3
gagandeeo <48759400+gagandeeo@users.noreply.github.com>
2021-06-12 22:34:32 +0530

    Update http-common.js

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const mlApi = axios.create({
-  baseURL: `http://localhost:8000/test/`,
+  baseURL: `http://fastml-alb-174465273.us-east-2.elb.amazonaws.com/api/test/`,
   headers: {
     "Content-type": "application/json",
   },
 });
 
 export const userApi = axios.create({
-  baseURL: `http://localhost:8000/test/`,
+  baseURL: `http://fastml-alb-174465273.us-east-2.elb.amazonaws.com/api/test/`,
 });
 
 export default { mlApi, userApi };
